562bc66feb7a057fd8b024c7d8c44543676809b7
Nick Pachulski <nick@pachulski.me>
2021-07-28 03:56:34 -0400

    Log the content offset

--- App.tsx
+++ App.tsx
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { FlatList, StyleSheet, Text, View } from "react-native"
+
+const data = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+]
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        onScroll={event =>
+          console.log("y position: " + event.nativeEvent.contentOffset.y)
+        }
+        scrollEventThrottle={16}
+        data={data}
+        renderItem={({ item: number }) => {
+          return <Text style={styles.row}>{number}</Text>
+        }}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 50,
   },
-});
+  row: {
+    textAlign: "center",
+    padding: 20,
+    width: "100%",
+    borderBottomWidth: 0.5,
+    borderBottomColor: "#000",
+  },
+})
